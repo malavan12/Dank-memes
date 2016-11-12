58b73dcc2088a152ae068d43a640c3eda7a09486
malavan12 <malavan14@hotmail.com>
2016-11-12 14:55:44 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,15 @@
+class Card
+{
+  String Name;
+  int Atk,Def,Level;
+  boolean Ability;
+  public Card(int a,int d,int l,String n,boolean ab)
+  {
+    Name=n;
+    Ability=ab;
+    Atk=a;
+    Def=d;
+    Level=l;
+  }
+  public boolean Summon(card A, card B) 
+}
